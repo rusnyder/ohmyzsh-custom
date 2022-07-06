468a4351377152b22d57d0e26c665fda53072d56
Russell Snyder <russ@arceo.ai>
2022-07-06 18:17:59 -0400

    Put ruby gems on the path

--- ruby.plugin.zsh
+++ ruby.plugin.zsh
@@ -1,5 +1,9 @@
 # Add Homebrew-installed ruby first to PATH (if avaialable)
 HOMEBREW_RUBY_BIN_DIR="${HOMEBREW_PREFIX}/opt/ruby/bin"
 if [[ -d "$HOMEBREW_RUBY_BIN_DIR" ]]; then
-  export PATH="${HOMEBREW_RUBY_BIN_DIR}:$PATH"
+  # Also put that Ruby install's gems on the path
+  RUBY_GEM_BIN_DIR="$(${HOMEBREW_RUBY_BIN_DIR}/gem environment gemdir)/bin"
+  export PATH="${HOMEBREW_RUBY_BIN_DIR}:${RUBY_GEM_BIN_DIR}:$PATH"
+  unset RUBY_GEM_BIN_DIR
 fi
+unset HOMEBREW_RUBY_BIN_DIR
